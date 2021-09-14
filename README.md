a3cad0983fdb7d54c949b71f81a9a3badfb32c25
Bruno Cabrita <bruno.cabrita@metacriacoes.com>
2021-09-14 15:51:24 +0100

    module first draft

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ```
 # run app
-SMTP_HOST=smtp.example.com SMTP_PORT=465 SMTP_USER="john.doe@example.com" SMTP_PASS="secret" REQUEST_TOKEN=abcd1234 deno run --allow-net --allow-env mod.ts
+DEBUG=true SMTP_HOST=smtp.example.com SMTP_PORT=587 SMTP_USER="john.doe@example.com" SMTP_PASS="secret" REQUEST_TOKEN=abcd1234 deno run --allow-net --allow-env mod.ts
 ```
--- mod.ts
+++ mod.ts
@@ -1,45 +1,113 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-// import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
+import { Application, Router, Context } from "https://deno.land/x/oak@v9.0.0/mod.ts";
+import { SmtpClient, SendConfig } from "https://deno.land/x/smtp@v0.7.0/mod.ts";
 
-const smtp = {
-  host: Deno.env.get("SMTP_HOST"),
-  port: Deno.env.get("SMTP_PORT"),
-  user: Deno.env.get("SMTP_USER"),
-  pass: Deno.env.get("SMTP_PASS"),
+interface SmtpConfig {
+  host: string;
+  port: number;
+  user: string;
+  pass: string;
+}
+
+interface RequestBody {
+  token: string;
+  from: string;
+  to: string;
+  cc?: string;
+  bcc?: string;
+  subject: string;
+  body: string;
+}
+
+const isDebug: boolean = Deno.env.get("DEBUG") === "true";
+const token: string = Deno.env.get("REQUEST_TOKEN") || 'abcd1234';
+const smtp: SmtpConfig = {
+  host: Deno.env.get("SMTP_HOST") || "smtp.example.com",
+  port: Number(Deno.env.get("SMTP_PORT")) || 587,
+  user: Deno.env.get("SMTP_USER") || "john.doe@example.com",
+  pass: Deno.env.get("SMTP_PASS") || "secret",
 };
 
-const token = Deno.env.get("REQUEST_TOKEN");
+async function sendSmtpEmail(data: SendConfig) {
 
-// const client = new SmtpClient();
+  const client = new SmtpClient();
+  
+  await client.connectTLS({
+    hostname: smtp.host,
+    port: smtp.port,
+    username: smtp.user,
+    password: smtp.pass,
+  });
+  
+  await client.send({
+    from: data.from,
+    to: data.to,
+    subject: data.subject,
+    content: data.content,
+    html: data.html,
+  });
+  
+  return await client.close();
+}
 
-// await client.connect({
-//   hostname: "smtp.163.com",
-//   port: 25,
-//   username: "username",
-//   password: "password",
-// });
+async function sendEmailRequest(ctx: Context) {
+  console.log(`${ctx.request.ip} @ ${new Date().toJSON()} → ${ctx.request.method}${ctx.request.url.pathname}`);
+    
+  if(!isDebug && !ctx.request.secure) {
+    ctx.response.status = 406;
+    ctx.response.body = `Request is not secure! Please make sure that the request url starts with "https://"`;
+    return;
+  }
 
-// await client.send({
-//   from: "mailaddress@163.com",
-//   to: "to-address@xx.com",
-//   subject: "Mail Title",
-//   content: "Mail Content",
-//   html: "<a href='https://github.com'>Github</a>",
-// });
+  if(!ctx.request.hasBody) {
+    ctx.response.status = 406;
+    ctx.response.body = `Invalid request body!`;
+    return;
+  }
 
-// await client.close();
+  const reqBody = ctx.request.body({type:"json"});
+
+  // @TODO validate request type: json
+
+  const body: RequestBody = await reqBody.value;
+
+  // @TODO: validate body fields
+
+  if(body?.token !== token) {
+    ctx.response.status = 406;
+    ctx.response.body = `Invalid token!`;
+    return;
+  }
+
+  ctx.response.type = "application/json; charset=utf-8";
+  
+  console.log("Sending email...");
+
+  try {
+
+    await sendSmtpEmail({
+      from: body.from,
+      to: body.to,
+      subject: body.subject,
+      content: "yada yada yada...",
+      html: body.body,
+    });
+
+  } catch(err) {
+
+    console.error(err);
+    return ctx.response.body = { success: false, error: err.message };
+    
+  }
+
+  return ctx.response.body = { success: true };
+}
+
+const router = new Router();
+router.post("/send-email", sendEmailRequest);
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = `Hello, world! ${token}`;
-});
-
-app.addEventListener("listen", ({ hostname, port, secure }) => {
-  console.log(
-    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
-      "localhost"}:${port}`,
-  );
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({port:8001});
